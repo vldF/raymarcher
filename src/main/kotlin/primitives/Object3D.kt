5f0220139785daedb906ddbf2a35715a5d386039
vldf <vladislavf7@gmail.com>
2020-12-06 00:00:08 +0300

    Minor changes

--- src/main/kotlin/primitives/Object3D.kt
+++ src/main/kotlin/primitives/Object3D.kt
@@ -7,4 +7,5 @@ abstract class Object3D {
     abstract var position: Vector3d
     abstract fun getDist(vec: Vector3d): Double
     abstract fun color(coords: Vector3d): ColorValue
+    open val isLightVisible = true
 }
